c9f0df1996a74348db72e6da700ec8ac14288a59
vadymchan <vadymchan@gmail.com>
2023-03-21 11:41:26 +0200

    added textures to models

--- engine/DirectX 11/source/Controller/TextureManager.h
+++ engine/DirectX 11/source/Controller/TextureManager.h
@@ -1,9 +1,13 @@
 #pragma once
-
-
+#include <unordered_map>
+#include "../Texture/Texture2D.h"
+#include "../Texture/DDSTextureLoader11.h"
+#include "../D3D/D3D.h"
 
 namespace engine::DX
 {
+	const std::wstring textureDirectory = L"engine\\general\\resources\\textures\\";
+
 
 	class TextureManager
 	{
@@ -15,8 +19,30 @@ namespace engine::DX
 			return instance;
 		}
 
-	private:
 
+		void addTexture2D(
+			const std::wstring& fileName,
+			const D3D11_TEXTURE2D_DESC& textureDesc,
+			size_t maxSize = 0,
+			DirectX::DDS_LOADER_FLAGS loadFlags = DirectX::DDS_LOADER_DEFAULT,
+			DirectX::DDS_ALPHA_MODE* alphaMode = nullptr)
+		{
+			assert(m_2dTextures.find(fileName) == m_2dTextures.end() && "Texture is already created! Error");
+			/*DirectX::CreateDDSTextureFromFileEx(g_device, g_devcon, fileName.c_str(), 0, )*/
+
+			
+
+			
+		}
+
+	private:
+		TextureManager() = default;
+		TextureManager(const TextureManager&) = delete;
+		TextureManager& operator=(const TextureManager&) = delete;
+
+		std::unordered_map<std::wstring, Texture2D> m_2dTextures{};
+		
+		
 	};
 
 }
--- engine/DirectX 11/source/Texture/Texture2D.h
+++ engine/DirectX 11/source/Texture/Texture2D.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../D3D/D3D.h"
+#include "DDSTextureLoader11.h"
 
 
 namespace engine::DX
@@ -8,7 +9,7 @@ namespace engine::DX
 	class Texture2D
 	{
 	public:
-		void initTexture(const D3D11_TEXTURE2D_DESC& textureDesc, const void* textureData, UINT memoryPitch = 0, UINT memorySlicePitch = 0)
+		void createTextureFromMemory(const D3D11_TEXTURE2D_DESC& textureDesc, const void* textureData, UINT memoryPitch = 0, UINT memorySlicePitch = 0)
 		{
 			initTextureDescription(textureDesc);
 			initTextureSubresourceData(textureData, memoryPitch, memorySlicePitch);
@@ -17,6 +18,31 @@ namespace engine::DX
 				g_device->CreateTexture2D(&m_textureDescription, &m_textureSubresourceData , texture.GetAddressOf());
 				textureUpdated = true;
 			}
+		}
+
+		void createTextureFromFile(
+			const std::wstring& fileName,
+			const D3D11_TEXTURE2D_DESC& textureDesc,
+			size_t maxSize = 0,
+			DirectX::DDS_LOADER_FLAGS loadFlags = DirectX::DDS_LOADER_DEFAULT,
+			DirectX::DDS_ALPHA_MODE* alphaMode = nullptr)
+		{
+
+			DirectX::CreateDDSTextureFromFileEx(
+				(ID3D11Device*)g_device,
+				(ID3D11DeviceContext*)g_devcon,
+				fileName.c_str(),
+				maxSize,
+				textureDesc.Usage,
+				(unsigned int)textureDesc.BindFlags,
+				(unsigned int)textureDesc.CPUAccessFlags,
+				(unsigned int)textureDesc.MiscFlags,
+				loadFlags,
+				(ID3D11Resource**)m_texture.GetAddressOf(),
+				m_shaderResourceView.GetAddressOf(),
+				alphaMode);
+
+		
 		}
 
 
@@ -36,8 +62,8 @@ namespace engine::DX
 			m_textureDescription.SampleDesc.Quality = 0;
 			m_textureDescription.Usage = D3D11_USAGE_DEFAULT;*/
 
-
-			m_textureDescription.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+			
+		
 			textureUpdated = false;
 		}
 
@@ -53,7 +79,9 @@ namespace engine::DX
 
 		D3D11_TEXTURE2D_DESC m_textureDescription{};
 		D3D11_SUBRESOURCE_DATA m_textureSubresourceData{}; //initial data
-		ComPtr<ID3D11Texture2D> texture;
+		ComPtr<ID3D11Texture2D> m_texture;
+		ComPtr<ID3D11ShaderResourceView> m_shaderResourceView{};
+
 		bool textureUpdated{};
 	};
 
